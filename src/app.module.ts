ed405f03a0e2345d278b7e5683352d8e7bc16b63
xeusteerapat <xeus085@gmail.com>
2022-09-12 21:55:18 +0700

    :wrench: [Update] update nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BrokerModule } from './broker/broker.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BrokerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
