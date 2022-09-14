2e270e0f0debd5f06357e565e7062ff3155db801
xeusteerapat <xeus085@gmail.com>
2022-09-14 11:52:54 +0700

    :wrench: [Update] configure gRPC microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'ChatMessage',
+      protoPath: join(__dirname, 'chat-message/chat-message.proto'),
+    },
+  });
+
+  await app.startAllMicroservices();
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
